8c7bf02fedc946a516eac43169163aeb9acac7b7
icevice <icevice24@gmail.com>
2024-10-26 18:26:53 +0700

    4.2. Заголовок в файле java [#159036 #124546]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Alexandr Begun");
+        System.out.println("27.08.1986");
+    }
+}
